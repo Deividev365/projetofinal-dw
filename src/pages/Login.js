69cdf901a60eba2e0909e5234fe75e1f6a2f6b8c
Deivid Almeida <deividalmeida365@gmail.com>
2021-12-03 23:02:57 -0300

    added login 1

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,49 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import { FiArrowLeft } from "react-icons/fi";
 
+
+import '../styles/pages/login.css'
+
+import logo from '../assets/logo.png'
 
 export function Login() {
     return(
+        <div className="App">
         
-        <div>
-            <h1>Aqui é a tela de login</h1>
-        </div>    
+        <img src={logo} className="logo" alt="Business view - Reports" />
 
-    )
+        <form className="form">
+
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" placeholder="email@gmail.com" />
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Senha</label>
+            <input type="password" name="password" />
+          </div>
+
+
+          <Link to="/app" className="primary">
+              
+                <p>ENTRAR</p> 
+
+          </Link>
+        
+        </form>
+        <Link to="/">
+        <FiArrowLeft size={58}></FiArrowLeft>
+        </Link>
+
+
+        <Link to="/" className="secondary"> 
+
+        </Link>
+
+
+      </div>
+    );
 }
 
